0800bcb5bc910c4416984a75260be9cad58bfc34
Kelly Hallman <khallman@tapmodo.com>
2018-08-31 16:01:26 -0700

    Updated Jcrop.vue component

--- src/components/Jcrop.vue
+++ src/components/Jcrop.vue
@@ -7,7 +7,7 @@ import Jcrop from 'jcrop';
 
 export default {
   name: 'Jcrop',
-  props: [ 'src', 'options' ],
+  props: [ 'src', 'options', 'rect' ],
   mounted: function() {
     const img = new Image();
     this.$el.appendChild(img);
@@ -18,22 +18,38 @@ export default {
     startup(img){
       console.log(this.options);
       this.jcrop = Jcrop.attach(img,this.options||{});
-      const rect = Jcrop.Rect.sizeOf(this.jcrop.el);
+      var rect = Jcrop.Rect.sizeOf(this.jcrop.el);
+
       this.jcrop.listen('crop.update',widget => this.$emit('update',widget));
       this.jcrop.listen('crop.activate',widget => this.$emit('activate',widget));
       this.jcrop.listen('crop.remove',widget => this.$emit('remove',widget));
-      this.jcrop.newWidget(rect.scale(.7,.5).center(rect.w,rect.h));
+
+      if (this.rect) rect = Jcrop.Rect.from(this.rect);
+      else rect = rect.scale(.7,.5).center(rect.w,rect.h)
+
+      this.jcrop.newWidget(rect);
       this.pos = this.jcrop.pos;
       this.jcrop.focus();
     }
   },
   watch: {
+    rect: {
+      handler: function(v) {
+        if (!this.jcrop.active) return false;
+        this.jcrop.active.animate(Jcrop.Rect.from(v),20,'inOutCirc')
+          .then(() => {
+            this.jcrop.focus();
+          });
+      },
+      deep: true
+    },
     options: {
       handler: function(o) { this.jcrop.setOptions(o); },
       deep: true
     }
   },
-  destroyed: () => {
+  beforeDestroy: function () {
+    this.jcrop.destroy();
   },
   data: () => ({
     pos: null,
